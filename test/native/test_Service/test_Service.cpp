c2926674ce1785e73f8733027f2fcd43d65f1d83
F-L-X-S <felixschuelke@icloud.com>
2024-07-03 18:43:26 +0200

    added testing getElement of content_stack

--- test/native/test_Service/test_Service.cpp
+++ test/native/test_Service/test_Service.cpp
@@ -2,7 +2,7 @@
 #include<unity.h>
 
 // Try to add more elements to an Content-Stack than the stack-size was initialized with 
-void test_stacksize(void) {
+void test_content_stack(void) {
     const uint8_t max_size = 3;                                // Max number of elements in stack 
     Content_stack<int, max_size> int_stack;                    // initialize int-stack for max 3 elements                                      
 
@@ -10,7 +10,7 @@ void test_stacksize(void) {
     int8_t size = 0;                                           // initialize size-counter for added elements
     for (size_t i = 0; i <= max_size+3; i++)                   // try to add more elements than possible 
     {
-        if (int_stack.addElement(i))
+        if (int_stack.addElement(i))                           // add int-elements with same value as index to stack 
         {
             size++;                                            // increase size-counter if element added successful
         }       
@@ -19,6 +19,22 @@ void test_stacksize(void) {
     char message[100];
     sprintf(message, "Added %d elements, but initialized stack with max-size %d", size, max_size);
     TEST_ASSERT_EQUAL_MESSAGE(max_size, size, message);        // Check stacksize (should be max)
+
+    // Get Item from Stack to check content 
+    memset(message, '\0', sizeof(message));                    // re-initialize assert-message
+    for (size_t i = 0; i < max_size+3; i++)
+    {
+        if (i<max_size)
+        {
+            TEST_ASSERT_EQUAL_MESSAGE(i, int_stack.getElement(i), "indexed element should contain the same value as positive index");    
+            TEST_ASSERT_EQUAL_MESSAGE(int_stack.getElement(i), int_stack.getElement(i-max_size), "pos. indexed element should be the same one as neg. indexed"); 
+        }
+        if (i>=max_size)
+        {
+            TEST_ASSERT_EQUAL_MESSAGE(false, int_stack.getElement(i), "indexing out-of-bounds should return false"); 
+        }
+    }
+    
 
     // Decrease Stacksize
     size = 0;                                                  // initialize size-counter for deleted elements
@@ -38,7 +54,7 @@ void test_stacksize(void) {
 
 int main(int argc, char **argv) {
     UNITY_BEGIN();
-    RUN_TEST(test_stacksize);
+    RUN_TEST(test_content_stack);
     UNITY_END();
 
     return 0;
