b9312efe907bf6a618e9cabae59ff9ed7c5f06b2
F-L-X-S <felixschuelke@icloud.com>
2024-07-05 10:25:26 +0200

    separated test for form_pdu and from_content

--- test/native/test_Message_service/test_Message_service.cpp
+++ test/native/test_Message_service/test_Message_service.cpp
@@ -7,37 +7,51 @@
 #define INSTANCE_ID 0xF
 
 
-// ---------------------------------------Test example instance of abstract Service-class---------------------------------------
-void test_Message(void) {
+// ---------------------------------------Test example instance of abstract Service-class constructed by content-struct---------------------------------------
+void test_Message_from_content(void) {
     //Create a sample message-content with Sender 0x1 and Receiver 0xF
     Message_content_t msg_content;
     msg_content.sender_id = 0x1;
     msg_content.receiver_id = 0xF;
     strcpy(msg_content.msg_text, "Hello");  
-    msg_content.txt_size = 5; 
+    msg_content.txt_size = sizeof(msg_content.msg_text); 
     Message msg_from_content(&msg_content);
 
+    // Check String-representation of created object
+    TEST_ASSERT_EQUAL_STRING_MESSAGE("Sender: 01\x09\x09Receiver: 0F\nHello\n", msg_from_content.to_string(),\
+    "Message-Object created with msg_from_pdu-constructor \
+    returns incorrect string-representation ");     
+
+    // Check PDU of created object
+    TEST_ASSERT_EQUAL_STRING_MESSAGE("\x01\x0F:Hello", msg_from_content.get_pdu(),\
+    "Message-Object created with msg_from_pdu-constructor \
+    returns incorrect pdu");     
+}
+
+// ---------------------------------------Test example instance of abstract Service-class constructed by PDU---------------------------------------
+void test_Message_from_pdu(void) {
+
     // Create a sample PDU with Sender 0x1 and Receiver 0xF
-    char sample_pdu[] = {0x1, 0xF, 'H', 'e', 'l', 'l', 'o'};
-    uint8_t pdu_size = 7; // Size of PDU
+    char sample_pdu[] = {0x1, 0xF, ':', 'H', 'e', 'l', 'l', 'o'};
+    uint8_t pdu_size = sizeof(sample_pdu); // Size of PDU
     Message msg_from_pdu(sample_pdu, &pdu_size);
 
 
-    // Check String-representation of both objects
-    TEST_ASSERT_EQUAL_STRING_MESSAGE(msg_from_pdu.to_string(), msg_from_content.to_string(),\
-    "Message-Object created with msg_from_content-constructor \
-    should return same string-representation as Message-Object created with msg_from_pdu-constructor");     
+    // Check String-representation of created object
+    TEST_ASSERT_EQUAL_STRING_MESSAGE("Sender: 01\x09\x09Receiver: 0F\nHello\n", msg_from_pdu.to_string(),\
+    "Message-Object created with msg_from_pdu-constructor \
+    returns incorrect string-representation ");     
 
-    // Check PDU of both objects 
-    TEST_ASSERT_EQUAL_STRING_MESSAGE(msg_from_pdu.get_pdu(), msg_from_content.get_pdu(),\
-    "Message-Object created with msg_from_content-constructor \
-    should return same pdu as Message-Object created with msg_from_pdu-constructor");     
-
+    // Check PDU of created object
+    TEST_ASSERT_EQUAL_STRING_MESSAGE("\x01\x0F:Hello", msg_from_pdu.get_pdu(),\
+    "Message-Object created with msg_from_pdu-constructor \
+    returns incorrect pdu");     
 }
 
 int main(int argc, char **argv) {
     UNITY_BEGIN();
-    RUN_TEST(test_Message);
+    RUN_TEST(test_Message_from_content);
+    RUN_TEST(test_Message_from_pdu);
     UNITY_END();
 
     return 0;
