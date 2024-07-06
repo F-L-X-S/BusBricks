cd496ea30b8ad26a3e42316dafdbdba4e0050bce
F-L-X-S <felixschuelke@icloud.com>
2024-07-06 21:42:17 +0200

    added ServiceBase-Class for Service-template

--- lib/Service/Service.h
+++ lib/Service/Service.h
@@ -13,13 +13,23 @@
 #include "Content.h"
 #include "Content_stack.h"
 
+// Service-Base-Class 
+class ServiceBase {
+public:
+    virtual char* get_response()=0;
+    virtual bool impart_pdu(char* pdu, uint8_t* pdu_size)=0;
+    virtual uint8_t* get_ServiceID()=0;
+    virtual uint8_t* get_InstanceID()=0;
+    virtual ~ServiceBase(){};
+};
+
 // Template to create a Service class by defining the Content (derived Class of "Content") to handle and the size of the service-stacks
 // A Service has to be instanciated with a unique modbus-rtu function code and an instance-id, the service-instance should use for it's communication
 template<typename content_class, int stackSize>                                       
-class Service{
+class Service: public ServiceBase{
 protected: 
-    uint8_t function_code;                                                                             // Modbus-rtu function code  
-    uint8_t instance_id;                                                                               // service-instance-id                                
+    uint8_t serviceID;                                                                                // service-id
+    uint8_t instanceID;                                                                               // service-instance-id                                
     Content_stack<content_class, stackSize> rec_stack;                                                 // stack for received content-elements
     char response_pdu[MAXPDUSIZE];                                                                     // PDU with response 
     
@@ -31,16 +41,26 @@ protected:
     }
 
 public: 
-    Service(uint8_t function_code, uint8_t instance_id): function_code(function_code), instance_id(instance_id){}  
+    Service(uint8_t serviceID, uint8_t instanceID): serviceID(serviceID), instanceID(instanceID){}  
+
+    // Get a Ptr to the Service-ID
+    uint8_t* get_ServiceID() override {
+        return &serviceID;
+    }
+
+    // Get a Ptr to the Instance-ID
+    uint8_t* get_InstanceID() override {
+        return &instanceID;
+    }
 
     // Add a new Content-Object created from PDU to the services receive-Stack
-    bool impart_pdu(char* pdu, uint8_t* pdu_size){                                                     
+    bool impart_pdu(char* pdu, uint8_t* pdu_size) override {                                                     
         content_class content(pdu, pdu_size);                                                          // create Content-object from PDU
         return rec_stack.addElement(content);                                                          // add Content-Object to Rec-Stack
     };
 
     // get the response-pdu stored at the reserved memory of the service instance  
-    virtual char* get_response(){                                                                      
+    virtual char* get_response() override {                                                                      
         // generic response element 
         content_class response_element;
         // write pdu from this element to response-pdu-memory of service-instance 
