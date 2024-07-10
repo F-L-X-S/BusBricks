f31d78e95b8cc42ce98ec872293e2e837a16f185
F-L-X-S <felixschuelke@icloud.com>
2024-07-10 19:25:56 +0200

    test_Message_servive with string pdu instead of char-pdu

--- test/manual/test_Message_service.cpp
+++ test/manual/test_Message_service.cpp
@@ -6,14 +6,14 @@ int main(){
     // initialize message-service with instance-id 0xF
     Message_service msg_service(INSTANCE_ID);       
     // Create a sample PDU with Sender 0x1 and Receiver 0xF
-    char sample_pdu[] = {0x1, INSTANCE_ID, ':', 'H', 'e', 'l', 'l', 'o'};
-    uint8_t pdu_size = 8; // Size of PDU
+    // Create a sample PDU with Sender 0x1 and Receiver 0xF
+    std::string sample_pdu = "\x01\x0F:" "Hello";
 
     // Impart PDU to Service till rec-stack is full
     bool recStack_not_full = true;
     while (recStack_not_full)
     {
-        recStack_not_full = msg_service.impart_pdu(sample_pdu, &pdu_size);
+        recStack_not_full = msg_service.impart_pdu(&sample_pdu);
         if (recStack_not_full)
         {
             std::cout<<"Added new PDU to Service Rec-Stack"<<std::endl;
@@ -23,8 +23,6 @@ int main(){
 
     // Ack all Messages in Receive-Stack 
     for (size_t i = 0; i < 5; i++) {
-        char response_pdu[MAXPDUSIZE];
-        strncpy(response_pdu, msg_service.get_response(), sizeof(response_pdu));
-        std::cout << "Response-PDU:\t" << i+1 << ": " << response_pdu << std::endl;
+        std::cout << "Response-PDU:\t" << i+1 << ": " << msg_service.get_response() << std::endl;
     }
 }
