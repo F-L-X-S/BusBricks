7128beaccc72240474b6547f4a7c3274682c2a5f
F-L-X-S <felixschuelke@icloud.com>
2024-07-25 17:46:24 +0200

    implemented addPDU_to_services

--- lib/ModbusRTU/ServiceInterface_modbusRTU.cpp
+++ lib/ModbusRTU/ServiceInterface_modbusRTU.cpp
@@ -14,6 +14,15 @@ void ServiceInterface_modbusRTU::getPDU_from_services()
 // Add all PDUs provided by the services to the sendstack
 void ServiceInterface_modbusRTU::addPDU_to_services()
 {
+    // Add all received PDUs to the Services 
+    while (!recStack.empty())
+    {
+        Frame_modbusRTU receivedFrame = recStack.getElement();
+        char ServiceID = receivedFrame.getFunctionCode();                             // Get the Modbus-RTU-Function-Code as Service-ID 
+        ServiceBase* destinationService = services->getService_byID(ServiceID);       // Pointer to the destination-Service 
+        std::string pdu = receivedFrame.getPDU();                                     // Get the Frames payload 
+        destinationService->impart_pdu(&pdu);                                         // Add a Content-Object created from PDU to the Services receive-stack 
+    };
 };
 
 
