49ead44533b8a9d72e99c9698cec064b65c4b768
F-L-X-S <felixschuelke@icloud.com>
2024-07-28 10:38:33 +0200

    Serial.print for debugging

--- lib/mockSoftwareSerial/mockSoftwareSerial.cpp
+++ lib/mockSoftwareSerial/mockSoftwareSerial.cpp
@@ -1,52 +1,57 @@
 #include "mockSoftwareSerial.h"
 
-arduinoMocking::SoftwareSerial::SoftwareSerial(int rx, int tx) : rxPin(rx), txPin(tx){};
+namespace arduinoMocking{
 
-void arduinoMocking::SoftwareSerial::begin(long baud) {
+    arduinoMocking::SoftwareSerial::SoftwareSerial(int rx, int tx) : rxPin(rx), txPin(tx){};
 
-};
+    void arduinoMocking::SoftwareSerial::begin(long baud) {
 
-void arduinoMocking::SoftwareSerial::println(const String &message) {
+    };
 
-};
+    void arduinoMocking::SoftwareSerial::println(const String &message) {
 
-void arduinoMocking::SoftwareSerial::print(const String &message) {
+    };
 
-};
+    void arduinoMocking::SoftwareSerial::print(const String &message) {
+
+    };
 
 
-void arduinoMocking::SoftwareSerial::write(char data) {
-    dataQueue.addElement(data);
-}
+    void arduinoMocking::SoftwareSerial::write(char data) {
+        dataQueue.addElement(data);
+    }
 
-void arduinoMocking::SoftwareSerial::flush(){
-    String result;
-    while (!dataQueue.empty()) {
-        result += *(dataQueue.getElement());
+    void arduinoMocking::SoftwareSerial::flush(){
+        String result;
+        while (!dataQueue.empty()) {
+            result += *(dataQueue.getElement());
+            dataQueue.deleteElement();
+        }
+        Serial.print("Flushed data:\t");
+        Serial.print(result);
+    }
+
+
+    int arduinoMocking::SoftwareSerial::read() {
+        if (dataQueue.empty()) {
+            return -1; 
+        }
+        char data = *(dataQueue.getElement());
         dataQueue.deleteElement();
+        return static_cast<int>(data);
     }
-    //std::cout << "Flushed data: " << result << std::endl;
-}
 
 
-int arduinoMocking::SoftwareSerial::read() {
-    if (dataQueue.empty()) {
-        return -1; 
+    int arduinoMocking::SoftwareSerial::peek() {
+        if (dataQueue.empty()) {
+            return -1; 
+        }
+        return static_cast<int>(*(dataQueue.getElement()));
     }
-    char data = *(dataQueue.getElement());
-    dataQueue.deleteElement();
-    return static_cast<int>(data);
-}
 
 
-int arduinoMocking::SoftwareSerial::peek() {
-    if (dataQueue.empty()) {
-        return -1; 
+    int arduinoMocking::SoftwareSerial::available() {
+        return (dataQueue.empty())?0:1;
     }
-    return static_cast<int>(*(dataQueue.getElement()));
-}
 
-
-int arduinoMocking::SoftwareSerial::available() {
-    return (dataQueue.empty())?0:1;
 }
