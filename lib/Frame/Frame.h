45b005b148b3e36ec871575b2a927174eea325ec
F-L-X-S <felixschuelke@icloud.com>
2024-08-18 12:41:13 +0200

    docs

--- lib/Frame/Frame.h
+++ lib/Frame/Frame.h
@@ -1,3 +1,14 @@
+/**
+ * @file Frame.h
+ * @author Felix Schuelke (flxscode@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-08-18
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #ifndef FRAME_H
 #define FRAME_H
 #ifdef ARDUINO
@@ -9,39 +20,66 @@
 
 #include<Content.h>
 
-// Typedefinitions for Constructor-parameters
+/** Typedefinitions for Constructor-parameters of the Frame-class 
+ * necessary to distinguish between construction from PDU (Content) or from byte-frame (Representation) 
+*/
 using pduString = String;
 using frameString = const char*;
 
-
-// Frame-Class as derived class from Content
-// The derived classes define:
-//      -the conversion from a given payload plus protocol-specific parameters (e.g. function-code, addresses...)
-//      to the frame-representation (override of content_to_rep from Content-Class)
-//
-//      - the extraction of protocol-specific parameters from the byte-representation (frame) 
-//      with eventually checking their validity (e.g. CRC) (override of rep_to_content from Content-Class)
-//
-// In the abstract concept of "Content" the byte-Frame itself is interpreted as the Content-Representation
-// The "Content"-Object represents the PDU transmitted to the services 
+/**
+ * @author Felix Schuelke (flxscode@gmail.com)
+ * @brief Frame-Class as derived class from Content
+The derived classes define:
+-the conversion from a given payload plus protocol-specific parameters (e.g. Modbus-function-code, addresses...)
+ to the frame-representation (override of content_to_rep from Content-Class)
+- the extraction of protocol-specific parameters from the byte-representation (frame) 
+ with eventually checking their validity (e.g. CRC) (override of rep_to_content from Content-Class
+In the abstract concept of "Content" the byte-Frame itself is interpreted as the Content-Representation
+The "Content"-Object represents the PDU transmitted to the services 
+ *  
+ * @tparam pduString type-definition for a String-Object containing the Payload of the frame ("Content")
+ * @tparam frameString type-definition for a const-char-pointer pointing to the byte-formatted frame 
+ */
 class Frame: public Content<pduString, frameString>{
     public:
-        // Default Constructor 
+       /**
+        * @brief Construct a new empty Frame object
+        * 
+        */
         Frame();
 
-        // Destrucor
+       /**
+        * @brief Destroy the Frame object
+        * 
+        */
         ~Frame();
 
-        // Constructor for creating Frame from pdu
+       /**
+        * @brief Constructor for creating Frame-instance from pdu (Content)
+        * 
+        * @param pdu pointer to pduString containing the payload of the frame
+        */
         Frame(pduString* pdu);
 
-        // Constructor for creating Frame-Object from byte-frame 
+       /**
+        * @brief Constructor for creating Frame-Object from byte-frame (Representation)
+        * 
+        * @param frame pointer to frameString containing the byte-frame (Representation) of the frame
+        */
         Frame(frameString* frame);
 
-        // get the pdu from the frame-object
+       /**
+        * @brief get the pdu (Content) as String from the frame-object
+        * 
+        * @return String with frame-payload
+        */
         String getPDU();
 
-        // get the byte-frame from the frame-object  
+       /**
+        * @brief Get the byte-frame (Representation) as String from the frame-object 
+        * 
+        * @return String with byte-frame (Representation)
+        */
         String getFrame();
 };
 #endif // FRAME_H
--- lib/Interface/CommInterface.h
+++ lib/Interface/CommInterface.h
@@ -1,3 +1,14 @@
+/**
+ * @file CommInterface.h
+ * @author Felix Schuelke (flxscode@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-08-18
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #ifndef COMMINTERFACE_H
 #define COMMINTERFACE_H
 #ifdef ARDUINO
@@ -9,65 +20,126 @@
 
 #include<Frame.h>
 
-// Communcation-Interface-Base-Class 
+/**
+ * @brief Communcation-Interface-Base-Class 
+ * containing functions, that have to be re-implemented in the derived classes  of the CommInterface-template 
+ * but which have to be called in other (abstract) classes (e.g. ServiceInterface)
+ */
 class CommInterfaceBase
 {
 public:
-    // Setup the Interface, has to be called in Setup-function
+    /**
+     * @brief Setup the Interface
+     *  has to be called in Setup-function
+     */
     void setup_interface(){};
 
-    // Add a new Frame to the send-buffer
+    /**
+     * @brief Add a new Frame to the send-buffer
+     * 
+     * @param sendFrame Pointer to String-Object with frame to be send next  
+     */
     void sendNewFrame(String* sendFrame){};
 
-    // Check, if the Frame was sent and the CommInterface is ready to send the next Frame 
+    /**
+     * @brief Check, if the Frame was sent and the CommInterface is ready to send the next Frame 
+     * 
+     * @return true Frame, that was added by sendNewFrame-function was send 
+     * @return false Frame, that was added by sendNewFrame-function is not sent yet 
+     */
     bool finishedSending(){};
 
-    // Define the destination, the next received Frame should be copied to 
+    /**
+     * @brief Define the destination, the next received Frame should be copied to ba a pointer to an empty String-Object
+     * 
+     * @param destFrameBuffer pointer to an empty String-Object, the next received frame should be stored in
+     */
     void getReceivedFrame(String* destFrameBuffer){};
 
-    // Check, if a new Frame was received 
+    /**
+     * @brief Check, if a new Frame was received 
+     * 
+     * @return true a new frame was received and stored on the with getReceivedFrame specified String-Object
+     * @return false no new frame was received, getReceivedFrame specified String-Object still empty
+     */
     bool receivedNewFrame(){};
 
-    // Execute the Communication-Cycle
-    void execCommunicationCycle(){};
+    /**
+     * @brief Construct a new Comm Interface Base object
+     * 
+     */
+    CommInterfaceBase(){};
 
-    CommInterfaceBase(/* args */){};
+    /**
+     * @brief Destroy the Comm-Interface Base object
+     * 
+     */
     ~CommInterfaceBase(){};
 };
 
-
-// Template for generic communication-interface
-// specifies a standardized interface to use for integrating 
-// any hardware-interface (e.g. SoftwareSerial, OneWire...)into a Service Interface 
-// The template ensures the correct handling of send- and receive-buffers of the interface:
-//
-// sendBuffer set to nullptr:       the Interface is ready to send another frame
-// sendBuffer not set to nullptr:   the frame, sendBuffer is pointing to, has to be send next 
-//
-// recBuffer set to nullptr:        the frame received last was written to the destination successfully, no new receive-Buffer was defined
-// recBuffer not set to nullptr:    the interface is waiting to receive a new frame to write ist to the destination, recBuffer is pointing to 
-
+/**
+    * @brief Template for generic communication-interface
+    * specifies a standardized interface to use for integrating 
+    * any hardware-interface (e.g. SoftwareSerial, OneWire...) into a Service-Interface 
+    * The template ensures the correct handling of send- and receive-buffers of the interface:
+    *
+    * sendBuffer set to nullptr:       the Interface is ready to send another frame
+    * sendBuffer not set to nullptr:   the frame, sendBuffer is pointing to, has to be send next 
+    *
+    * recBuffer set to nullptr:        the frame received last was written to the destination successfully, no new receive-Buffer was defined
+    * recBuffer not set to nullptr:    the interface is waiting to receive a new frame to write it to the destination, recBuffer is pointing to 
+    * 
+    * @tparam interface_type type of native bus-interface to setup the Comm-interface for (e.g. SoftwareSerial)
+*/
 template<typename interface_type>                                       
 class CommInterface: public CommInterfaceBase{
     protected:
-        interface_type* interface;
-        String* sendBuffer = nullptr;                              // set to nullptr if Frame was sent 
-        String* receiveBuffer = nullptr;                           // set to nullptr if Frame was copied to destination
+        /// @brief pointer to an instance of the native bus-interface (setup outside of BusBricks)
+        interface_type* interface;                       
+
+        /// @brief pointer to the next frame to be send, set to nullptr if Frame was sent 
+        String* sendBuffer = nullptr;                              
+
+        /// @brief pointer to String-object, a received frame should be stored at, set to nullptr if Frame was copied to destination
+        String* receiveBuffer = nullptr;                           
 
         // Send a Frame 
         // True, if a frame was send successfully 
-        virtual bool send() = 0;                                        // implementation of sending a Frame has to be done in the derived class
+
+        /**
+         * @brief Send the frame, sendBuffer is pointing to
+         * (has to be done in the derived class)
+         *
+         * @return true the frame, the sendBuffer was pointing to, was sent 
+         * @return false the frame, the sendBuffer was pointing to, was not sent or sendBuffer nullptr (no frame to be sent)
+         */
+        virtual bool send() = 0;                                       
         
 
-        // Receive a Frame 
-        // True, if a new Frame was received
-        virtual bool receive() = 0;                                     // implementation of receiving a Frame has to be done in the derived class
+        /**
+         * @brief Receive a Frame 
+         * (has to be done in the derived class)
+         * 
+         * @return true a new Frame was received and stored at the String-obj, receiveBuffer is pointing to 
+         * @return false no new frame was received receiveBuffer still empty
+         */
+        virtual bool receive() = 0;                                     
 
     public:
-        // Construct Communication-Interface 
+        /**
+         * @brief Construct a new Comm-Interface object
+         * 
+         * @param interface pointer to an instance of the native bus-interface (setup outside of BusBricks)
+         * @param baudrate bits per second
+         */
         CommInterface(interface_type* interface, uint32_t baudrate): interface(interface){}; 
 
-        // Add a new Frame to the send-buffer
+        /**
+         * @brief Specify the next frame to be sent by the interface, 
+         * no overwrite, if the sendBuffer is already pointing to another frame
+         * 
+         * @param sendFrame String-pointer to next frame to be send
+         */
         void sendNewFrame(String* sendFrame){
             if (sendBuffer == nullptr)
             {
@@ -75,34 +147,57 @@ class CommInterface: public CommInterfaceBase{
             }
         };
 
-        // Check, if the Frame was sent and the CommInterface is ready to send the next Frame 
+        /**
+         * @brief Check, if the Frame was sent and the CommInterface is ready to send the next Frame 
+         * Interface is ready to send a new frame, if the sendBuffer is nullpointer 
+         * 
+         * @return true ready to accept a new frame to send next with sendNewFrame-function
+         * @return false frame, sendBuffer is pointing to is not sent yet
+         * not ready to to accept a new frame in sendNewFrame-function
+         */
         bool finishedSending(){
             return sendBuffer==nullptr;
         };
 
-        // Define the destination, the next received Frame should be copied to 
+        /**
+         * @brief Define the destination, the next received Frame should be stored at 
+         * 
+         * @param destFrameBuffer pointer to String-object, the next received Frame should be stored at 
+         */
         void getReceivedFrame(String* destFrameBuffer){
             receiveBuffer = destFrameBuffer;                            // set the receive-buffer-ptr to the destination, the next received Frame should be stored at 
         };
 
-        // Check, if a new Frame was received 
+        /**
+         * @brief Check, if a new Frame was received and stored at the in getReceivedFrame specified location
+         * the interface is waiting waiting for getReceivedFrame-call to specify the location, the next received frame should be stored at
+         * 
+         * @return true a new frame was received and stored, waiting for getReceivedFrame-call to specify receiveBuffer
+         * @return false receiveBuffer is specified, but still empty
+         */
         bool receivedNewFrame(){
             return receiveBuffer==nullptr;
         };
 
-        // Execution of receive-cycle
+        /**
+         * @brief Execution of receive-cycle, set the receiveBuffer to nullptr after a new frame was received 
+         * 
+         */
         virtual void receiveCycle(){                                     
             // Receiving
             if (receive()) {
-                receiveBuffer = nullptr;                                // set the receive-buffer to nullptr after a new frame was received 
+                receiveBuffer = nullptr;                                
             };
         };
         
-        // Execution of send-cycle
+        /**
+         * @brief Execution of send-cycle, set the sendBuffer to nullptr, after the frame, sendBuffer was pointing at, was send 
+         * 
+         */
         virtual void sendCycle(){   
             // Sending
             if (send()){
-                sendBuffer = nullptr;                                   // set the send-buffer to nullptr after the frame, pointed to, was send 
+                sendBuffer = nullptr;                                   
             }; 
         };
 };
