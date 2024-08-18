2d4358e9a639e3cc0cc144fc48ae437a86bc7614
F-L-X-S <felixschuelke@icloud.com>
2024-08-18 20:18:41 +0200

    docs

--- lib/ModbusRTU/CommInterface_modbusRTU.h
+++ lib/ModbusRTU/CommInterface_modbusRTU.h
@@ -1,3 +1,14 @@
+/**
+ * @file CommInterface_modbusRTU.h
+ * @author Felix Schuelke (flxscode@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-08-18
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #ifndef COMMINTERFACE_MODBUSRTU
 #define COMMINTERFACE_MODBUSRTU
 #ifdef ARDUINO
@@ -13,39 +24,99 @@
 
 #include <CommInterface.h>
 
+/// @brief debugging flag to prit debugging-information on Serial 
 #define DEBUG
 
-#define MAXFRAMESIZE 256 //Max number of Bytes a frame can contain
+/// @brief maximum number of Bytes a frame can contain
+#define MAXFRAMESIZE 256 
 
-// The Modbus-RTU CommInterface-class specifies a communication interface with SoftwareSerial as interface-type
-// Monitors modbus-specific timeouts 
-// Ensures correct data-transfer between standardized Frame-Objects in the Interface-Stack and SoftwareSerial
-// Does NOT inspect any frame-Content (task of the Frame-class) 
+/**
+ * @brief The Modbus-RTU CommInterface-class specifies a communication interface with SoftwareSerial as interface-type
+ * and Monitors modbus-specific timeouts. Ensures correct data-transfer between standardized Frame-Objects in the Interface-Stack and SoftwareSerial.
+ * 
+ */
 class CommInterface_modbusRTU: public CommInterface<SoftwareSerial>{
     public:
-        // Construct Communication-Interface with predefined SoftwareSerial
+        /**
+         * @brief Construct a new CommInterface_modbusRTU object 
+         * 
+         * @param softwareserial pointer to the SoftwareSerial-instance, the Comminterface should use  
+         * @param baudrate baudrate in bps to calculate timeouts 
+         * @param deviceId Modbus-specific device-identifier 
+         */
         CommInterface_modbusRTU(SoftwareSerial* softwareserial ,uint16_t baudrate, char deviceId);
 
-        // Destroy Communication-Interface
+        /**
+         * @brief Destroy the CommInterface_modbusRTU object
+         * 
+         */
         ~CommInterface_modbusRTU();
 
-        // Send the Frame from Send-buffer
+        /**
+         * @brief Override of CommInterface send-function. 
+         * Send the frame, sendBuffer is pointing to by writing and flushing SoftwareSerial-interface
+         * 
+         * @return true frame was flushed to serial-bus
+         * @return false sendBuffer is nullptr
+         */
         bool send() override;
 
-        // Receive a Frame and write it to Receive-buffer
+        /**
+         * @brief Receives a Modbus RTU frame and writes it to `receiveBuffer`.
+         *
+         * Waits for incoming bytes for a duration defined by `_recTimeout`. If no bytes are received within this period, the function exits. 
+         * When bytes are received via SoftwareSerial, the function starts writing the frame to `receiveBuffer` once a byte containing the 
+         * device ID of the `CommInterface` instance is detected. If the device ID is the null terminator (master mode), all incoming frames are captured.
+         * Bytes related to the frame must be received within the `_charTimeout`.
+         * 
+         * The function continues receiving until either `MAXFRAMESIZE` is reached or `_charTimeout` expires. After receiving, it waits for `_frameTimeout` 
+         * before clearing the SoftwareSerial receive buffer by calling `_clearRxBuffer`.
+         * 
+         * @return `true` if a frame was successfully received and written to `receiveBuffer` within the receive timeout.
+         * @return `false` if no frame was received during `_recTimeout`.
+         * 
+         * @details The function operates in two modes:
+         * - **Slave Mode**: Begins receiving only if the frame is addressed to the device's ID.
+         * - **Master Mode**: Captures any incoming frame.
+         * 
+         * The function does not validate the content of the received frame. It uses timeouts to ensure the entire frame is received and then clears the communication interface's buffer.
+         * 
+         * @note If `DEBUG` is defined, debugging information is printed to the serial monitor.
+         */
         bool receive() override;
 
-        // Get the Modbus-Device-ID 
+        /**
+         * @brief Get Modbus-specific device-identifier
+         * 
+         * @return char Modbus-specific device-identifier
+         */
         char getDeviceId();
 
     private:
-        const char deviceId;                            // Modbus-RTU Slave ID, default nullterminator for mastermode
-        unsigned long _charTimeout;                     // Timeout between two chars received 
-        unsigned long _frameTimeout;                    // min time between two Frames
-        unsigned long _recTimeout = 1000000;            // time to wait for response during the receive-cycle
+        /// @brief Modbus-RTU Slave ID, default nullterminator for mastermode
+        const char deviceId;       
 
-        void _calculateTimeouts(uint16_t baud);         // Calculate all timeouts based on the specified baudrate
-        void _clearRxBuffer();                          // Clear the receive-buffer 
+        /// @brief Timeout between two chars received [microseconds]                   
+        unsigned long _charTimeout;                     
+
+        /// @brief  minimum time to wait between two Frames [microseconds]      
+        unsigned long _frameTimeout;                    
+
+        /// @brief time to wait for response during the receive-cycle [microseconds]      
+        unsigned long _recTimeout = 1000000;            
+
+        /**
+         * @brief Calculate all timeouts based on the specified baudrate
+         * 
+         * @param baud bus-baudrate [bps]
+         */
+        void _calculateTimeouts(uint16_t baud);         
+
+        /**
+         * @brief Clear the receive-buffer of SoftwareSerial 
+         * 
+         */
+        void _clearRxBuffer();                          
 
 };
 
--- lib/ModbusRTU/Frame_modbusRTU.h
+++ lib/ModbusRTU/Frame_modbusRTU.h
@@ -1,3 +1,14 @@
+/**
+ * @file Frame_modbusRTU.h
+ * @author Felix Schuelke (flxscode@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-08-18
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #ifndef FRAME_MODBUSRTU_H
 #define FRAME_MODBUSRTU_H
 #ifdef ARDUINO
@@ -9,67 +20,185 @@
 
 #include<Frame.h>
 
+/// @brief two bytes prefix in frame before pdu (Device-adress, modbus-functioncode)
+#define PREFIXSIZE 2    
 
-#define PREFIXSIZE 2    // two bytes Prefix (Device-adress, modbus-functioncode)
-#define SUFFIXSIZE 2    // two bytes Suffix (CRC HighByte / LowByte)
+/// @brief two bytes suffix in frame after pdu (CRC HighByte / LowByte)
+#define SUFFIXSIZE 2    
+
+/// @brief CRC-16 mask to use (modbus-rtu default 0xA001)
 #define CRC16MASK 0xA001
+
+/// @brief CRC-16 startvalue to use (modbus-rtu default 0xFFFF)
 #define CRC16VALUE 0xFFFF
 
-// Class for defining the structure of the Modbus-RTU-Frame
-// Constructors:
-// Construction by PDU: 
-// Frame_modbusRTU(pduString* pdu, char* slaveId, char* functionCode)
-// Object calculates and adds the CRC16, 
-// the Modbus-Slave-ID and the Modbus-Function-Code and adds them to the Frame-representation. 
-// Construction by Frame-representation:
-// Frame_modbusRTU(frameString* frame)
-// Object extracts Payload, Slave-ID and Function-Code and checks the CRC16 for plausibility by checking rest = 0. 
-// 
-// because the frame-representation is stored as a reference, the representation has to be allocated on the heap-memory (buffer)
-// The allocation is done during the conversion from "Content" (construction by PDU) within the content_to_rep-function
-// or it has to be initiated by calling the function "copy_to_heap" (e.g. in copy-constructor or during construction by frame-representation)
+
+
+/**
+ * @brief Class for defining the structure of the Modbus-RTU-Frame. Can be constructed by a given modbus-frame and extracts payload, 
+ * device-id and function-code (additionally checking CRC) or by providing payload, device-id and function-code (additionally calculating CRC).
+ * 
+ * Because the frame-representation is stored as a reference, the representation has to be allocated on the heap-memory (buffer)
+ * The allocation is done during the conversion from "Content" (construction by PDU) within the content_to_rep-function
+ * or it has to be initiated by calling the function "copy_to_heap" (e.g. in copy-constructor or during construction by frame-representation)
+ * 
+ */
 class Frame_modbusRTU: public Frame{
     public:
         // Construct Modbus-RRU-Frame from given PDU (Content)
+        /**
+         * @brief Construct a new Frame_modbusRTU object by PDU, modbus-slave-ID and modus-function-code. 
+         * Object calculates and adds the CRC16, the Modbus-Slave-ID and the Modbus-Function-Code and
+         * concatenates them to the modbus-frame (representation) by calling content_to_rep during construction. 
+         * Allocate heap-memory (buffer) to the frame-representation and let the Content's representation (parent-class-template)
+         * point to that buffer on heap.
+         * 
+         * @param pdu 
+         * @param slaveId 
+         * @param functionCode 
+         */
         Frame_modbusRTU(pduString* pdu, char* slaveId, char* functionCode);
 
-        // Construct Modbus-RTU-Frame from given Byte-Frame (Representation)
+        /**
+         * @brief Construct Modbus-RTU-Frame from given Byte-Frame (Representation). Copy the representation, the argument is pointing to,
+         * to heap-memory and let by calling copy_to_heap and let Content's representation (parent-class-template) point to that buffer on heap.
+         * Extract Payload, device-ID and function-Code. Check the CRC16 for plausibility by checking rest = 0. 
+         * 
+         * @param frame const char** to a modbus-rtu-frame to construct the Frame-object from
+         */
         Frame_modbusRTU(frameString* frame);
 
-        // Construct empty Modbus-RTU-Frame 
+        /**
+         * @brief Construct empty Modbus-RTU-Frame 
+         * 
+         */
         Frame_modbusRTU();
 
-        // Deconstructor 
+        /**
+         * @brief Destroy the Frame_modbusRTU object
+         * 
+         */
         ~Frame_modbusRTU();
 
-        // Copy constructor
+        /**
+         * @brief Copy constructor.
+         *
+         * Initializes a new Frame_modbusRTU object as a copy of another Frame_modbusRTU object.
+         * 
+         * @param other The other Frame_modbusRTU object to copy.
+         * 
+         * @details This constructor performs a deep copy of all the data from `other`, 
+         * ensuring that the new object is an exact copy of the source object.
+         */
         Frame_modbusRTU(const Frame_modbusRTU& other); 
 
-        // Assignment operator
+        /**
+         * @brief Assignment operator.
+         *
+         * This operator assigns the values from another object of the same type to the current object.
+         * 
+         * @param other The other Frame_modbusRTU object whose values will be assigned.
+         * @return A reference to the current object after assignment.
+         * 
+         * @details This operator performs a deep copy of the data from `other`, ensuring that the current object 
+         * becomes an exact copy of `other`.
+         */
         Frame_modbusRTU& operator=(const Frame_modbusRTU& other);
 
-        // Check the CRC16-value for teh given buffer 
+        /**
+         * @brief Verifies the CRC16 checksum of the current Modbus RTU frame.
+         *
+         * This function calculates the CRC16 checksum of the frame's representation and checks 
+         * whether it is correct.
+         * 
+         * @return true if the CRC16 checksum is correct, `false` otherwise.
+         * 
+         * @details The function creates a temporary buffer, copies the frame's representation to it, 
+         * and then compares the calculated CRC16 checksum with the expected value. 
+         * The temporary buffer is deleted after use.
+         */
         bool checkCRC16();
 
-        // Get the Modbus-RTU-function-code of the frame-instance 
+        /**
+         * @brief Get the Modbus-RTU-function-code of the frame-instance 
+         * 
+         * @return char Modbus-RTU-function-code of the frame-instance 
+         */
         char getFunctionCode();
 
     private:
+        /// @brief  Modbus-RTU-slave-id (device-id)
         char slaveId = '\0';
+
+        /// @brief Modbus-RTU-function-code
         char functionCode = '\0';
+
+        /// @brief buffer for allocating heap-memory of dynamic length to the frames-representation 
         char* buffer = new char[1];
 
-        // Convert the given Content (PDU) to Representation (Frame)
+        /**
+         * @brief Converts the given Content (PDU) to the Modbus RTU frame representation.
+         *
+         * This function creates a buffer containing the full Modbus RTU frame, 
+         * which includes the slave address, function code, content (PDU), and the calculated CRC16 checksum.
+         * The resulting frame is stored in the `representation` member.
+         * 
+         * @details The buffer is dynamically allocated based on the size of the content (PDU) plus space for 
+         * the prefix (slave address and function code) and suffix (CRC16). The buffer is null-terminated 
+         * and assigned to the `representation`. The function also handles memory management by deleting 
+         * any previously allocated buffer before creating a new one.
+         */
         void content_to_rep()  override;
 
-        // Convert the given Representation (Frame) to Content (PDU) 
+        /**
+         * @brief Converts the given frame representation (Modbus RTU frame) to the Content (PDU).
+         *
+         * This function extracts the content (PDU) from the Modbus RTU frame representation, 
+         * along with the slave ID and function code. The content is extracted only if the frame 
+         * is long enough to contain a valid PDU.
+         * 
+         * @details If the frame is shorter than the expected minimum size (prefix + suffix), 
+         * the content will remain empty. The slave ID and function code are extracted from the first 
+         * two bytes of the frame, and the PDU content is extracted from the bytes in between the prefix 
+         * and suffix. 
+         * 
+         * @note The function assumes that the representation is null-terminated.
+         */
         void rep_to_content() override;
 
-        // Calculate the CRC16-value of the given buffer 
+        /**
+         * @brief Calculates the CRC16 checksum of the given buffer.
+         *
+         * This function computes the CRC16 checksum for the provided buffer using the standard 
+         * Modbus RTU CRC16 algorithm. The calculation is performed byte-by-byte, applying bitwise 
+         * operations to generate the checksum.
+         * 
+         * @param crcBuffer A pointer to the buffer containing the data for which the CRC16 checksum is calculated.
+         * @param size The size of the buffer in bytes.
+         * @return The calculated CRC16 checksum as an unsigned short.
+         * 
+         * @details The CRC16 algorithm uses a specific initialization value and applies a polynomial 
+         * mask during the calculation. The function iterates over each byte in the buffer, updating the 
+         * checksum according to the Modbus RTU CRC16 standard.
+         */
         unsigned short calcCRC16(char* buffer, uint8_t size);
 
-        // Copy the representation pointing to an outside defined char-array to heap-memory
-        // has to be called to ensure, that destructor is deleting from heap and not from stack
+        /**
+         * @brief Copies the representation pointing to an externally defined char-array to heap memory.
+         *
+         * This function ensures that the provided string, which may reside in stack memory, 
+         * is copied to heap memory. This is necessary to ensure that the destructor correctly 
+         * frees memory from the heap and avoids attempting to delete stack-allocated memory.
+         * 
+         * @param str_ptr A double pointer to the externally defined char-array that will be copied to heap memory.
+         * 
+         * @details The function allocates a new buffer on the heap, copies the contents of the given 
+         * string into this buffer, and ensures null-termination. After copying, it updates the pointer 
+         * reference to point to the newly allocated heap memory.
+         * 
+         * @note The function deletes any previously allocated heap memory pointed to by `buffer` 
+         * before allocating the new memory.
+         */
         void copy_to_heap(const char** str_ptr);
 
     };
