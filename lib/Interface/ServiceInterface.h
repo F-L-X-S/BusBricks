0bebfcfcd32fe1637b1a85eef2da2f140bd62c50
F-L-X-S <felixschuelke@icloud.com>
2024-08-18 13:21:55 +0200

    docs

--- lib/Interface/ServiceInterface.h
+++ lib/Interface/ServiceInterface.h
@@ -1,3 +1,14 @@
+/**
+ * @file ServiceInterface.h
+ * @author Felix Schuelke (flxscode@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-08-18
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #ifndef SERVICEINTERFACE_H
 #define SERVICEINTERFACE_H
 #ifdef ARDUINO
@@ -16,46 +27,76 @@
 #define DEBUG
 
 
-// Template for generic Service-Interface
-// The derived class defines the handling of incoming frames from communication-interface to the services (associated by service-id)
-// and the conversion from PDU provided by Services to frames getting sent by the communication-interface
-// Template-parameters: 
-//  -type of the Communication-Interface (e.g. SoftwareSerial, Wire...)
-//  -type of the frames to be send as a derived Class of the Frame-Class (e.g. Frame_modbusRTU)
+/**
+ * @brief Template for generic Service-Interface
+ * Defines the handling of incoming frames from CommInterface to services stored in the associated ServiceCluster
+ * and the conversion from PDU provided by Services in the ServiceCluster to frames, getting sent by the communication-interface.
+ * 
+ * @tparam CommInterfaceBase type of CommInterface to use, derived Class of the abstract CommInterface-Class (e.g. CommInterface_modbusRTU)
+ * @tparam frameType type of the frames to be send, derived Class of the abstract Frame-Class (e.g. Frame_modbusRTU)
+ */
 template<typename CommInterfaceBase, typename frameType>                                       
 class ServiceInterface{
     public:
-        // Constructor for Service-Interface 
+        /**
+         * @brief Construct a new Service Interface object
+         * 
+         * @param comm_interface pointer to an instance of a CommInterface-derived class-object 
+         * @param services pointer to an instance of a ServiceCluster-derived class-object containing the associated services 
+         */
         ServiceInterface(CommInterfaceBase* comm_interface, ServiceClusterBase* services):
                 comm_interface(comm_interface), 
                 services(services) {};
 
-        // Execute all relevant tasks for transferring data between CommInterface and Services:
-        // - Get PDU from Services
-        // - Add PDU to Services
-        // - Update communication-stacks
-        // - Execute the CommInterfaces Comm-cycle 
-        //
-        // Has to be implemented in the derived class
+        /**
+         * @brief Execute all relevant tasks for transferring data between CommInterface and Services:
+         * Getting payloads to sent from the Services, adding received payloads to the right Services,
+         * Updating communication-stacks, Executing the CommInterfaces send- and receive-cycle
+         * (Has to be implemented in the derived class)
+         * 
+         */
         virtual void communicate() = 0;        
 
     protected:
-        CommInterfaceBase* comm_interface;                          // Communication-Interface 
-        ServiceClusterBase* services;                               // ServiceCluster containing the Services associated with the Interface 
-        Content_stack<frameType, STACKSIZE> recStack;               // stack for received frames
-        Content_stack<frameType, STACKSIZE> sendStack;              // stack for frames to send next
-        String sendItem;                                            // Item to be sent next
-        String recItem;                                             // Item received last
+        /// @brief pointer to Communication-Interface (instance of CommInterface-derived class)
+        CommInterfaceBase* comm_interface;        
 
-        // Add all PDUs provided by the services to the sendstack
-        // Implemented in derived Class, depending on frametype
+        /// @brief pointer to ServiceCluster, containing the Services associated with the Interface (instance of ServiceCluster-derived class)             
+        ServiceClusterBase* services;                               
+
+        /// @brief stack for received frames (instance of Content_stack-derived class, specified for type of frames, the bus is using)    
+        Content_stack<frameType, STACKSIZE> recStack;     
+
+        /// @brief stack for frames to send next (instance of Content_stack-derived class, specified for type of frames, the bus is using)        
+        Content_stack<frameType, STACKSIZE> sendStack;        
+
+        /// @brief Item to be sent next     
+        String sendItem;  
+
+        /// @brief Item received last                                          
+        String recItem;                                             
+
+        /**
+         * @brief Add all PDUs provided by the services to the sendstack,
+         * depending on how frametype stores the information, which service the payload is designated for
+         * (Implemented in derived Class)
+         * 
+         */
         virtual void getPDU_from_services()=0;
 
-        // Impart all received PDUs from the receivestack to the associated services 
-        // Implemented in derived Class, depending on frametype
+        /**
+         * @brief Impart all received PDUs from the receivestack to the associated services, 
+         * depending on how frametype stores the information, which service the payload is designated for
+         * (Implemented in derived Class)
+         * 
+         */
         virtual void addPDU_to_services()=0;
 
-        // start the processing of the registered services 
+
+        /**
+         * @brief start the stackProcessing of the registered services 
+         * 
+         */
         virtual void processServices(){
             for (size_t i = 0; i < services->getNumberOfServices(); i++)
                 {
@@ -64,8 +105,11 @@ class ServiceInterface{
                 }
         }
 
-        // Add the last item from the sendstack to the Comminterface
-        // delete it after sending
+        /**
+         * @brief Add items from the sendStack to the comm_interface and execute the sendCycle of CommInterface
+         * delete the sent item from the sendStack
+         * 
+         */
         virtual void processSendStack(){
             // Serial debugging
             #ifdef DEBUG
@@ -85,8 +129,10 @@ class ServiceInterface{
             };        
         }
 
-
-        // Add items received by the CommInterface to the Receivestack
+        /**
+         * @brief Add items received by the CommInterface to the recStack and execute the CommInterface's receiveCycle to wait for new incoming frames
+         * exit, if the recStack is full or the CommInterface did not received new Frames within the in receiveCycle specified timeout
+         */
         virtual void processRecStack() {
             // Serial debugging
             #ifdef DEBUG
