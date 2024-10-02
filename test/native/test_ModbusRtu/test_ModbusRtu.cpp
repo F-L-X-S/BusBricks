37fa06b3d387056c7978308c7d820df702276441
F-L-X-S <felixschuelke@icloud.com>
2024-10-02 21:03:22 +0200

    test for modbusRTU Frame

--- test/native/test_ModbusRtu/test_ModbusRtu.cpp
+++ test/native/test_ModbusRtu/test_ModbusRtu.cpp
@@ -22,55 +22,55 @@
  */
 
 #include <Frame_modbusRTU.h>
-#include<unity.h>
+#include <unity.h>
 
 #define SLAVEID '9'
 #define FUNCTIONCODE 'm'
-#define EXMPL_PDU "123 Hello to the World!"                     
-#define EXMPL_FRAME "9m123 Hello to the World!\xA5\x82"                                 // Frame with calculated CRC 
+#define EXMPL_PDU "123 Hello to the World!"                                               // Example Payload to use in frame
+#define EXMPL_FRAME "9m123 Hello to the World!\xA5\x82"                                   // Frame with calculated CRC for example-payload 
 
 // ---------------------------------------Test Modbus-RTU Frame---------------------------------------
 void test_Frame_modbusRTU(void) {
+    // Check empty Frame 
+    Frame_modbusRTU* framePtr = new  Frame_modbusRTU();
+
+    TEST_ASSERT_EQUAL_STRING_MESSAGE("", framePtr->get_content()->c_str(),                // Check Frame PDU 
+    "PDU returned from empty Frame-Obj is not empty");  
+
+    TEST_ASSERT_EQUAL_MESSAGE(4, (framePtr->get_representation()->getSize()),             // Check empty Frame-CharArray  
+    "CharArray of empty frame has not size 4 (Device-Id+functioncode+CRC+CRC)");   
+  
+    delete framePtr;                                                                      // Call Destructor (test-error if failed)
+
+
     // Devicesettings 
     char slaveId = SLAVEID;
     char functionCode = FUNCTIONCODE;
-
-    // Check empty Frame 
-    Frame_modbusRTU* framePtr = new  Frame_modbusRTU();
-    TEST_ASSERT_EQUAL_STRING_MESSAGE("", (framePtr->getPDU()).c_str(),                  // Check empty Frame  
-    "PDU returned from empty Frame-Obj is not empty");   
-
-    TEST_ASSERT_EQUAL_STRING_MESSAGE("", (framePtr->getFrame()).c_str(),                // Check Frame-Byte-Representation
-    "Byte-Representation (Frame) returned from empty Frame-Obj is not empty");    
-
-    delete framePtr;                                                                    // Call Destructor (test-error if failed)
-
     // Create a Modbus-RTU Frame from the Message-PDU 
-    pduString msg_pdu = EXMPL_PDU;
+    String msg_pdu = EXMPL_PDU;
     Frame_modbusRTU frame_from_pdu(&msg_pdu, &slaveId, &functionCode);
     
-    TEST_ASSERT_EQUAL_STRING_MESSAGE(EXMPL_PDU, frame_from_pdu.getPDU().c_str(),                // Check Frame PDU 
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(EXMPL_PDU, frame_from_pdu.get_content()->c_str(),               // Check Frame PDU 
     "PDU returned from Frame-Obj created by frame-representation does not match PDU the Frame was initialized with");    
 
-    TEST_ASSERT_EQUAL_STRING_MESSAGE(EXMPL_FRAME, frame_from_pdu.getFrame().c_str(),            // Check Frame-Byte-Representation
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(EXMPL_FRAME, frame_from_pdu.get_representation()->getData(),    // Check Frame-Byte-Representation
     "Byte-Representation (Frame) returned from Frame-Obj created by PDU does not match the expected Frame");    
 
-    TEST_ASSERT_TRUE_MESSAGE(frame_from_pdu.checkCRC16(),                                       // Check, if the CRC-calculation for the whole Frame returns zero as CRC
-    "CRC16 Rest fo whole Frame is not zero");                                                   // otherwise the CRC in the Frame is incorrect 
+    TEST_ASSERT_TRUE_MESSAGE(frame_from_pdu.checkCRC16(),                                            // Check, if the CRC-calculation for the whole Frame returns zero as CRC
+    "CRC16 Rest fo whole Frame is not zero");                                                        // otherwise the CRC in the Frame is incorrect 
 
-    // Create a new Frame-Object from Frame above
-    std::string frame = frame_from_pdu.getFrame();
-    frameString frame_ptr = frame.c_str();
-    Frame_modbusRTU frame_from_frame(&frame_ptr);
+    // Create a new Frame-Object from the representation (CharArray)
+    CharArray frameRepresentation = String(EXMPL_FRAME);
+    Frame_modbusRTU frame_from_rep(&frameRepresentation);
 
-    TEST_ASSERT_EQUAL_STRING_MESSAGE(EXMPL_PDU, frame_from_frame.getPDU().c_str(),              // Check Frame PDU 
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(EXMPL_PDU, frame_from_rep.get_content()->c_str(),              // Check Frame PDU 
     "PDU returned from Frame-Obj created by frame-representation does not match PDU the Frame was initialized with");    
 
-    TEST_ASSERT_EQUAL_STRING_MESSAGE(EXMPL_FRAME, frame_from_frame.getFrame().c_str(),          // Check Frame-Byte-Representation
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(EXMPL_FRAME, frame_from_rep.get_representation()->getData(),   // Check Frame-Byte-Representation
     "Byte-Representation (Frame) returned from Frame-Obj created by frame-representation does not match the expected Frame");    
 
-    TEST_ASSERT_TRUE_MESSAGE(frame_from_frame.checkCRC16(),                                     // Check, if the CRC-calculation for the whole Frame returns zero as CRC
-    "CRC16 Rest fo whole Frame created by frame-representation is not zero");                                                   // otherwise the CRC in the Frame is incorrect 
+    TEST_ASSERT_TRUE_MESSAGE(frame_from_rep.checkCRC16(),                                           // Check, if the CRC-calculation for the whole Frame returns zero as CRC
+    "CRC16 Rest fo whole Frame created by frame-representation is not zero");                       // otherwise the CRC in the Frame is incorrect 
 
 }
 
